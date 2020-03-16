b0b27b14704126b6b9f408cace9e1e79f10b41a4
lisa1am <laitmouloud@gmail.com>
2020-03-16 13:19:09 +0000

    added  to print for clearer dislay

--- hash/main.py
+++ hash/main.py
@@ -1,7 +1,7 @@
 import sha256
 
 def printHash(element):
-    print(element, "=>", sha256.hash(element))
+    print("\"",element,"\"", " => ","\"", sha256.hash(element),"\"")
 
 
 testList=["","a","abc","message digest","abcdefghijklmnopqrstuvwxyz","ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789","12345678901234567890123456789012345678901234567890123456789012345678901234567890""The quick brown fox jumps over the lazy dog"]
